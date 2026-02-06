c4cec667f7665d1d0b63226217a260ddcfd48700
Estebxz <estebanmendezm1995@gmail.com>
2026-02-05 23:04:49 -0500

    archivo robots.txt añadido ademas de Sitemap en configuracion, se edito meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svgo: true
   },
-
+  
   output: "static",
+  site: "https://joanmm.netlify.app",
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    sitemap({
+      entryLimit: 10000
+    })
+  ]
 });
