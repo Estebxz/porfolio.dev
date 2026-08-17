68dd6a24470b0833dd1c847cd8c10d66e3877dc6
Estebxz <estebanmnendezm1995@gmail.com>
2026-08-16 20:33:18 -0500

    PERF: Optimizar prioridad de carga en imagenes y fuentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    svgo: true,
-  },
-
   prefetch: true,
   output: "static",
   site: "https://joanmm.netlify.app",
